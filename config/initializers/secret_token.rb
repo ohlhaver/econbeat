efd4dd9e9c3ab6b0873c97fcb2c2eb863835dfd9
Justus Ohlhaver <ohlhaver@gmail.com>
2012-08-14 16:36:14 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jurnalo::Application.config.secret_token = '33ec24304d7617472e4a1e6545bdb2cf3a950b183824ac01b136c929c6f17763ff74e240e55688369dd6b960572b4defe6298511100631759a2826c390af516d'
