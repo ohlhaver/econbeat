40885c14a1a424e601d181d215dccab31c9c956d
Justus Ohlhaver <ohlhaver@gmail.com>
2012-08-23 15:23:20 +0200

    unique indexs for names and emails in user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120822142351) do
+ActiveRecord::Schema.define(:version => 20120822171944) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -99,6 +99,9 @@ ActiveRecord::Schema.define(:version => 20120822142351) do
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
 
   create_table "utopics", :force => true do |t|
     t.integer  "user_id"
