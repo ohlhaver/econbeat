c2f2ffb37803093506c81fbc2eb05b7644fbc19b
Justus Ohlhaver <ohlhaver@gmail.com>
2012-08-27 23:05:44 +0200

    fix bug for homepage feed view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,12 @@ class StaticPagesController < ApplicationController
       @topics = Topic.find(:all)
 
       if params[:topic]
-        @feed_items = current_user.feed.where("topic_id = ?", params[:topic]).page params[:page] 
+        #@feed_items = current_user.feed.where("topic_id = ?", params[:topic])
+        @feed_items = current_user.feed.find_all{|v| v.topic_id == params[:topic].to_i } 
       else
-        @feed_items = current_user.feed.page params[:page]
+        @feed_items = current_user.feed
       end
+        @feed_items = Kaminari.paginate_array(@feed_items).page(params[:page])
     end
  
   end
