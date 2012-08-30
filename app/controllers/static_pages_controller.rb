59f154a48a9a26542e2973493a41f443b0e833ca
Justus Ohlhaver <ohlhaver@gmail.com>
2012-08-30 19:00:34 +0200

    fix bug for home topics

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,21 +4,19 @@ class StaticPagesController < ApplicationController
     if current_user
       @post  = current_user.posts.build
       @topics = Topic.find(:all)
-
-      if params[:topic]
-        #@feed_items = current_user.feed.where("topic_id = ?", params[:topic])
-        @feed_items = current_user.feed.find_all{|v| v.topic_id == params[:topic].to_i } 
-      else
-        @feed_items = current_user.feed
-        @display_topics=[]
+      @feed_items = current_user.feed
+      @display_topics=[]
         @topics.each do |topic|
           topic_feed_items = @feed_items.find_all{|i| i.topic_id == topic.id}
           @display_topics = @display_topics << topic if topic_feed_items.size >= 1
         end
-        @topics = @display_topics
+      @topics = @display_topics
 
-
+      if params[:topic]
+        #@feed_items = current_user.feed.where("topic_id = ?", params[:topic])
+        @feed_items = current_user.feed.find_all{|v| v.topic_id == params[:topic].to_i } 
       end
+
         @feed_items = Kaminari.paginate_array(@feed_items).page(params[:page])
 
 
