76f01968ff6294a3e42fcd58552215c0e0ce1994
Justus Ohlhaver <ohlhaver@gmail.com>
2012-08-30 18:51:09 +0200

    limit displayed topics to ones with posts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,8 +10,19 @@ class StaticPagesController < ApplicationController
         @feed_items = current_user.feed.find_all{|v| v.topic_id == params[:topic].to_i } 
       else
         @feed_items = current_user.feed
+        @display_topics=[]
+        @topics.each do |topic|
+          topic_feed_items = @feed_items.find_all{|i| i.topic_id == topic.id}
+          @display_topics = @display_topics << topic if topic_feed_items.size >= 1
+        end
+        @topics = @display_topics
+
+
       end
         @feed_items = Kaminari.paginate_array(@feed_items).page(params[:page])
+
+
+
     end
  
   end
