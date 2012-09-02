68f3b3fbdc8e5e68a20d7841f7a485a8497145cf
Justus Ohlhaver <ohlhaver@gmail.com>
2012-09-02 22:01:08 +0200

    connect rt updates with fb_sync

--- app/models/facebook_update.rb
+++ app/models/facebook_update.rb
@@ -1,3 +1,9 @@
 class FacebookUpdate < ActiveRecord::Base
   attr_accessible :id, :type
+  after_create :draw_realtime_posts
+
+  def draw_realtime_posts
+  	@user = User.find_by_uid(self.uid)
+  	@user.delay.sync_fb
+  end
 end
