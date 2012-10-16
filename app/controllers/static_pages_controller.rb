fcc09851f42d4796e10fd31d2edd8d41f7290ad7
Justus Ohlhaver <ohlhaver@gmail.com>
2012-10-16 13:01:09 +0200

    add uncommented code for only showing the latest user action on any given article or post

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,41 +3,18 @@ class StaticPagesController < ApplicationController
   def home
     if current_user
       @starred_actions = current_user.starred_author_action_feed.where("created_at >= :time", {:time => 1.day.ago}).sort_by{|e| -e[:id]}
+      
+      #@user_actions = current_user.user_action_feed
+      #@unique_user_actions =[]
+      #@unique_user_actions += Array.wrap(@user_actions.first)
+      #@user_actions.each do |a|
+      #  @unique_user_actions += Array.wrap(a) if @unique_user_actions.find_all{|i| i.article_id == a.article_id}==[]
+      #end
+
+      # @actions = (@unique_user_actions + current_user.author_action_feed - @starred_actions).sort_by{|e| -e[:id]}
+
       @actions = (current_user.user_action_feed + current_user.author_action_feed - @starred_actions).sort_by{|e| -e[:id]}
-      #@articles = current_user.articles
-      #@topics = Topic.find(:all)
-      #@topics = @topics.find_all{|i| i.posts.where(:hidden=>nil).count > 0}
-      #@feed_items = current_user.feed
-      #@display_topics=[]
-      #  @topics.each do |topic|
-      #    topic_feed_items = @feed_items.find_all{|i| i.topic_id == topic.id}
-      #    @display_topics = @display_topics << topic if topic_feed_items.size >= 1
-      #  end
-      #@topics = @display_topics
-
-      #if params[:topic]
-        #@feed_items = current_user.feed.where("topic_id = ?", params[:topic])
-      #  @feed_items = current_user.feed.find_all{|v| v.topic_id == params[:topic].to_i } 
-      #end
-
-     # @feed_items.each do |i|
-     #     if i.fbaction_id 
-     #       object = current_user.facebook.get_object(i.fbaction_id) 
-     #       if object
-     #         c = object["comments"]
-     #         i.comments = c["data"] if c
-     #         #i.count = c["count"] if c
-     #       else
-     #         i.box= false
-     #       end
-     #     else
-     #       i.box= false
-     #     end
-
-
-      #end
-
-        @actions = Kaminari.paginate_array(@actions).page(params[:page]).per(50)
+      @actions = Kaminari.paginate_array(@actions).page(params[:page]).per(50)
 
 
 
