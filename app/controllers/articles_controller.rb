f99acccd1f15df6e3b0d95b5a9a7baf492149789
Justus Ohlhaver <ohlhaver@gmail.com>
2012-10-22 11:51:48 +0200

    better formating and related authors on the article pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,21 @@ class ArticlesController < ApplicationController
 		@actions = @article.catcher.author.actions - @action
 		@actions = Kaminari.paginate_array(@actions).page(params[:page]).per(50)
 
+		if current_user
+			@friends = current_user.followed_users
+			@subscribed_friends = @friends.find_all{|i| i.subscribed?(@author)}.first(10)
+		end
+		@all_authors =[]
+		@size = @author.users.size
+		@author.users.each do |u|
+			@all_authors += (u.authors - Array.wrap(@author))
+		end
+
+		@author_hash = @all_authors.inject(Hash.new(0)) { |h,n| h[n] += 1; h }
+		@author_hash = @author_hash.sort_by {|author, count| -count}
+		@ranked_authors = @author_hash.to_a
+		@recommended = @ranked_authors.first(3)
+
 	end	
 
 	def comment
