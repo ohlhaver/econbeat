bf09f48f90092f67220e8da1b0ffe16204bef31a
Justus Ohlhaver <ohlhaver@gmail.com>
2012-10-22 22:50:33 +0200

    retract last change

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,7 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
-      user.email = auth.info.email unless auth.info.email.blank?
+      user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.picture = user.facebook.get_picture("me")
