583f0d76fc704ee6f9fa8cd2cc91f4f1799a0f4b
Justus Ohlhaver <ohlhaver@gmail.com>
2012-10-28 16:19:38 +0100

    popup for todays articles

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,32 +1,4 @@
 module ActionsHelper
-	def ppopover_link author
-		
-
-  		if current_user.subscribed?(author)
-
-	  	 	form_for(current_user.subscribed?(author),
-	             html: { method: :delete }) do |f| 
-	   			f.submit "Unsubscribe", class: "btn btn-link"
-			end 
-
-			if current_user.subscribed?(author).starred == true
-				
-       
-          		link_to raw('<i class="icon-star"></i>'), "/subscriptions/#{current_user.subscriptions.find_by_author_id(author.id).id}/unstar", :class => "btn btn-small", title: 'click to unstar', rel: 'tooltip', "data-placement"=>"bottom"
-        	else
-        		
-
-          		link_to raw('<i class="icon-star-empty"></i>'), "/subscriptions/#{current_user.subscriptions.find_by_author_id(author.id).id}/star", :class => "btn btn-small", title: 'click to star this author', rel: 'tooltip', "data-placement"=>"bottom"
-          
-        	end
-		else
-			form_for(current_user.subscriptions.build(author_id: author.id)) do |f|
- 				f.hidden_field :author_id
-
-  				f.submit "Subscribe", class: "btn btn-link" 
-			end
-		end
-	end
 
 	def popover_link author
 		@author=author
@@ -35,18 +7,10 @@ module ActionsHelper
 	end
 
 
-
-def ppopover_link author
-		
-
-  	 if current_user.subscribed?(author)
-
-  	 	 button_to "unsubscribe", :controller=>"subscriptions", :action => "unsubscribe", :id => author.id, :class => "btn btn-link"
-
-	else
-		button_to "subscribe", :controller=>"subscriptions", :action => "subscribe", :id => author.id, :class => "btn btn-link"
-
-	end
+def today author
+	@todays_actions=author.actions.where("actions.created_at >= :time", {:time => 1.day.ago})
+	render 'authors/today'	 
+	
 end
 
 def sender action
